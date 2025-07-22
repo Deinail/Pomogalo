5f1643e46ec95590832252f67249f96f15c74663
Deinail <cssrudom@gmail.com>
2025-07-22 17:15:11 +0500

    edn brake_calc

--- src/Trail.java
+++ src/Trail.java
@@ -1,20 +1,31 @@
+import java.util.Scanner;
+
 public class Trail { // Параметры поезда
 
-    public int trailWeght; // Вес поезда
-    public int locoWeight; // Вес локомотива
-    public int weight; // Общий вес поезда
+        public int trailWeght; // Вес поезда
+        public int locoWeight; // Вес локомотива
+        public int fullWeight; // Общий вес поезда
+        public boolean trailStatus; // Провереяем гружёный состав либо порожний
 
     public void inputValue(){ //Ввод данных поезда от пользователя
 
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите вес поезда : ");
+        trailWeght = input.nextInt();
+        System.out.print("\nВведите вес локомотива : ");
+        locoWeight = input.nextInt();
+        System.out.print("\nСостав: true)гружёный, false)порожний : ");
+        trailStatus = input.nextBoolean();
 
     }
 
-    public int fullWeight(int trail, int loco) { // Расчёт полного веса поезда
+    public int calcWeight() { // Расчёт полного веса поезда
 
-        weight = trail + loco;
+        inputValue();
+        fullWeight = trailWeght + locoWeight;
+        System.out.println("\nОбщий вес поезда : " + fullWeight);
 
-        return weight;
+        return fullWeight;
 
     }
 
